1b7f111134599eaffc7f2de85b9bb63d2ef5fa17
EvgeniiMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2020-04-15 15:17:03 +0300

    Fixed label on result window

--- Second laboratory work/Lab2(Machine Int.)/ExperSystemResult.Designer.cs
+++ Second laboratory work/Lab2(Machine Int.)/ExperSystemResult.Designer.cs
@@ -94,9 +94,9 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.Location = new System.Drawing.Point(12, 85);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(284, 15);
+            this.label3.Size = new System.Drawing.Size(277, 15);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Правило, по которому было получен результат:";
+            this.label3.Text = "Правило, по которому был получен результат:";
             // 
             // ExpertSystemResult
             // 
