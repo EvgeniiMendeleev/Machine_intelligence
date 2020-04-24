8e13db2a9a903a383e5bb500a228672dc45c2258
EvgeniiMendeleev <43094600+EvgeniiMendeleev@users.noreply.github.com>
2020-04-24 20:31:15 +0300

    Fix

---
+++ Third laboratory work/FramesKnowledges/FramesKnowledges/Frame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FramesModel
+{
+    enum PointerOnAttribute {  }
+    enum TypeOfData { FRAME, INTEGER, REAL, BOOL, LISP, TEXT, LIST }
+    struct Slot
+    {
+        string name;
+    }
+
+    class Frame
+    {
+        string name;
+        PointerOnAttribute ptr;
+        List<Slot> slots = new List<Slot>();
+    }
+}
